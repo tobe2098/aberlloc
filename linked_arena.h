67c4e344e1139f48c04f2795d452762cfc195eda
Antoni Bertolin <tonibm1220@gmail.com>
2025-03-18 10:30:12 +0100

    Constructor destructor

--- linked_arena.h
+++ linked_arena.h
@@ -1,8 +1,9 @@
-#ifndef _LINKED_ARENA_HEADER
-#define _LINKED_ARENA_HEADER
+#ifndef _LINKEDV_ARENA_HEADER
+#define _LINKEDV_ARENA_HEADER
 // #include <pthread.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "./static_arena.h"
 #include "./utils.h"
 #ifdef _WIN32
 #ifdef __GNUC__
@@ -17,19 +18,21 @@
 #endif
 // Fixed size arena, only manages power of two alignments based on word size (if not power of 2, error)
 // Single-threaded
-typedef struct LinkedArena {
-    uint8_t*  memory_;      // Base pointer to reserved memory
-    uintptr_t position_;    // Current allocation position
+typedef struct LinkedVArena {
+    uint8_t*  memory_;    // Base pointer to reserved memory
+    uintptr_t position_;  // Current allocation position
+    uintptr_t committed_size_;
     uintptr_t total_size_;  // Size
     // pthread_mutex_t __arena_mutex;
     int auto_align_;
     int alignment_;
-    // LinkedArena*    __parent;
-} LinkedArena;
 
-int Init_StaticArena(LinkedArena* arena, int arena_size, int auto_align) {
+    LinkedVArena* next_arena_;
+} LinkedVArena;
+
+int Init_ParentLinkedVArena(LinkedVArena* arena, int arena_size, int auto_align) {
 #ifdef DEBUG
-  if (arena == NULL) {
+  if (arena == NULL || arena_size < _getPageSize()) {
     return ERROR_INVALID_PARAMS;
   }
 #endif
@@ -44,29 +47,43 @@ int Init_StaticArena(LinkedArena* arena, int arena_size, int auto_align) {
     arena->auto_align_ = FALSE;
     arena->alignment_  = word_size;
   }
-  arena->memory_ = os_new_virtual_mapping_(arena_size);
+  arena->committed_size_ = _getPageSize();
+  arena->memory_         = os_new_virtual_mapping_(arena->total_size_);
+  arena->next_arena_     = NULL;
   if (arena->memory_ == NULL) {
+    return ERROR_OS_MEMORY;
+  }
+  if (os_commit_(arena->memory_, arena->committed_size_) == ERROR_OS_MEMORY) {
+    os_free_(arena->memory_, arena->total_size_);
     return ERROR_OS_MEMORY;
   }
   return SUCCESS;
 }
-int Destroy_StaticArena(LinkedArena* arena) {
+// Here
+int Destroy_LinkedVArena(LinkedVArena* arena) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
   }
 #endif
-  if (os_free_(arena->memory_, arena->total_size_) == ERROR_OS_MEMORY) {
-    return ERROR_OS_MEMORY;
+  while (arena->next_arena_ != NULL) {
+    Destroy_LinkedVArena(arena);
   }
-  arena->memory_     = NULL;
-  arena->total_size_ = 0;
-  arena->position_   = 0;
-  arena->auto_align_ = 0;
+  if (os_free_(arena->memory_, arena->total_size_) == ERROR_OS_MEMORY) {
+    DEBUG_PRINT("Freeing old virtual memory did not work during remap. Memory leaked.");
+  }
+
+  arena->memory_         = NULL;
+  arena->total_size_     = 0;
+  arena->committed_size_ = 0;
+  arena->position_       = 0;
+  arena->auto_align_     = 0;
+  arena->alignment_      = 0;
+  arena->next_arena_     = NULL;
   return SUCCESS;
 }
 
-int SetAutoAlign2Pow_StaticArena(LinkedArena* arena, int alignment) {
+int SetAutoAlign2Pow_LinkedVArena(LinkedVArena* arena, int alignment) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
@@ -80,7 +97,63 @@ int SetAutoAlign2Pow_StaticArena(LinkedArena* arena, int alignment) {
   return SUCCESS;
 }
 
-uintptr_t GetPos_StaticArena(LinkedArena* arena) {
+int ReMap_LinkedVArena(LinkedVArena* arena, int total_size) {
+#ifdef DEBUG
+  if (total_size < arena->committed_size_) {
+    // Need to ensure there is enough space at destination of memcopy
+    return ERROR_INVALID_PARAMS;
+  }
+#endif
+  uint8_t* new_memory = os_new_virtual_mapping_(total_size);
+  if (new_memory == NULL) {
+    return ERROR_OS_MEMORY;
+  }
+  if (os_commit_(new_memory, total_size) == ERROR_OS_MEMORY) {
+    if (os_free_(new_memory, total_size) == ERROR_OS_MEMORY) {
+      DEBUG_PRINT("Freeing new virtual memory block did not work during destruction. Virtual memory leaked.");
+    }
+    return ERROR_OS_MEMORY;
+  }
+  memcpy(new_memory, arena->memory_, arena->position_);
+  // We cannot tolerate failure after this, as we have two blocks of memory to manage. It has to be freed
+  if (os_free_(arena->memory_, arena->total_size_) != 0) {
+    DEBUG_PRINT("Freeing old virtual memory did not work during destruction. Memory leaked.");
+  }
+  arena->memory_ = new_memory;
+  return SUCCESS;
+}
+
+int ExtendCommit_LinkedVArena(LinkedVArena* arena, int total_commited_size) {
+#ifdef DEBUG
+  if (!arena || !total_commited_size || total_commited_size < arena->committed_size_) {
+    return ERROR_INVALID_PARAMS;
+  }
+#endif
+  if (total_commited_size > arena->total_size_) {
+    DEBUG_PRINT("Not enough virtual memory in the arena, remapping.");
+    if (!ReMap_LinkedVArena(arena, extendPolicy(arena->total_size_))) {
+      DEBUG_PRINT("Remap failed, not enough memory.");
+      return ERROR_OS_MEMORY;
+    }
+  }
+  // We only need to extend the memory commitment under the total size.
+  if (os_commit_(arena->memory_, total_commited_size) == ERROR_OS_MEMORY) {
+    return ERROR_OS_MEMORY;
+  }
+  arena->committed_size_ = total_commited_size;
+}
+int ReduceCommit_LinkedVArena(LinkedVArena* arena, int total_commited_size) {
+#ifdef DEBUG
+  if (!arena || !total_commited_size || total_commited_size > arena->__commited_size) {
+    return ERROR_INVALID_PARAMS;
+  }
+#endif
+  if (os_uncommit_(arena->memory_ + total_commited_size, arena->committed_size_ - total_commited_size) == ERROR_OS_MEMORY) {
+    return ERROR_OS_MEMORY;
+  }
+  arena->committed_size_ = total_commited_size;
+}
+uintptr_t GetPos_LinkedVArena(LinkedVArena* arena) {
 #ifdef DEBUG
   if (arena == NULL) {
     return NULL;
@@ -89,7 +162,7 @@ uintptr_t GetPos_StaticArena(LinkedArena* arena) {
   return arena->position_;
 }
 
-int PushAligner_StaticArena(LinkedArena* arena, int alignment) {
+int PushAligner_LinkedVArena(LinkedVArena* arena, int alignment) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
@@ -98,11 +171,12 @@ int PushAligner_StaticArena(LinkedArena* arena, int alignment) {
     return ERROR_INVALID_PARAMS;
   }
 #endif
-  arena->position_ = align_2pow(arena->position_ + (uintptr_t)arena->memory_, alignment) - (uintptr_t)arena->memory_;
+  arena->position_ = align_2pow(arena->position_, alignment);
+  // arena->position_ = align_2pow(arena->position_ + (uintptr_t)arena->__memory, alignment) - (uintptr_t)arena->__memory;
   return SUCCESS;
 }
 
-int PushAlignerCacheLine_StaticArena(LinkedArena* arena) {
+int PushAlignerCacheLine_LinkedVArena(LinkedVArena* arena) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
@@ -112,33 +186,48 @@ int PushAlignerCacheLine_StaticArena(LinkedArena* arena) {
   return SUCCESS;
 }
 
-uint8_t* PushNoZero_StaticArena(LinkedArena* arena, int bytes) {
+int PushAlignerPageSize_LinkedVArena(LinkedVArena* arena) {
+#ifdef DEBUG
+  if (arena == NULL) {
+    return ERROR_INVALID_PARAMS;
+  }
+#endif
+  arena->position_ = align_2pow(arena->position_ + (uintptr_t)arena->memory_, _getPageSize()) - (uintptr_t)arena->memory_;
+  return SUCCESS;
+}
+
+uint8_t* PushNoZero_LinkedVArena(LinkedVArena* arena, int bytes) {
 #ifdef DEBUG
   if (arena == NULL) {
     return NULL;
   }
 #endif
   if (arena->auto_align_) {
-    PushAligner_StaticArena(arena, arena->alignment_);
+    PushAligner_LinkedVArena(arena, arena->alignment_);
   }
-  if (arena->position_ + bytes > arena->total_size_) {
-    return NULL;
+  while (arena->position_ + bytes > arena->committed_size_) {
+    if (ExtendCommit_LinkedVArena(arena, extendPolicy(arena->committed_size_)) == ERROR_OS_MEMORY) {
+      return NULL;
+    }
   }
   uint8_t* ptr = arena->memory_ + arena->position_;
   arena->position_ += bytes;
   return ptr;
 }
-uint8_t* Push_StaticArena(LinkedArena* arena, int bytes) {
+uint8_t* Push_LinkedVArena(LinkedVArena* arena, int bytes) {
 #ifdef DEBUG
   if (arena == NULL) {
     return NULL;
   }
 #endif
   if (arena->auto_align_) {
-    PushAligner_StaticArena(arena, arena->alignment_);
+    PushAligner_LinkedVArena(arena, arena->alignment_);
   }
-  if (arena->position_ + bytes > arena->total_size_) {
-    return NULL;
+
+  while (arena->position_ + bytes > arena->committed_size_) {
+    if (ExtendCommit_LinkedVArena(arena, extendPolicy(arena->committed_size_)) == ERROR_OS_MEMORY) {
+      return NULL;
+    }
   }
   uint8_t* ptr = arena->memory_ + arena->position_;
   arena->position_ += bytes;
@@ -146,7 +235,7 @@ uint8_t* Push_StaticArena(LinkedArena* arena, int bytes) {
   return ptr;
 }
 
-int Pop_StaticArena(LinkedArena* arena, uintptr_t bytes) {
+int Pop_LinkedVArena(LinkedVArena* arena, uintptr_t bytes) {
   // Be careful, if auto align is on, the aligner allocated bytes are unseen to you. You should use pop to position or address if autoalign
   // is on.
 #ifdef DEBUG
@@ -158,9 +247,14 @@ int Pop_StaticArena(LinkedArena* arena, uintptr_t bytes) {
     bytes = arena->position_;
   }
   arena->position_ -= bytes;
+  while (arena->position_ > _getPageSize() && reduceCondition(arena->committed_size_, arena->position_)) {
+    if (ReduceCommit_LinkedVArena(arena, reducePolicy(arena->committed_size_)) == ERROR_OS_MEMORY) {
+      DEBUG_PRINT("Reduce commit in Virtual arena failed");
+    }
+  }
   return SUCCESS;
 }
-int PopTo_StaticArena(LinkedArena* arena, uintptr_t position) {
+int PopTo_LinkedVArena(LinkedVArena* arena, uintptr_t position) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
@@ -169,38 +263,53 @@ int PopTo_StaticArena(LinkedArena* arena, uintptr_t position) {
   if (position < arena->position_) {
     arena->position_ = position;
   }
+  while (arena->position_ > _getPageSize() && reduceCondition(arena->committed_size_, arena->position_)) {
+    if (ReduceCommit_LinkedVArena(arena, reducePolicy(arena->committed_size_)) == ERROR_OS_MEMORY) {
+      DEBUG_PRINT("Reduce commit in Virtual arena failed");
+    }
+  }
   return SUCCESS;
 }
-int PopToAdress_StaticArena(LinkedArena* arena, uint8_t* address) {
+int PopToAdress_LinkedVArena(LinkedVArena* arena, uint8_t* address) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
   }
 #endif
   uintptr_t final_position = address - arena->memory_;
-  if ((uintptr_t)(arena->memory_) < (uintptr_t)address) {
+  if ((uintptr_t)(arena->memory_) < (uintptr_t)address || (uintptr_t)(arena->memory_) + arena->position_ > (uintptr_t)address) {
     arena->position_ = final_position;
+  } else {
+    DEBUG_PRINT("Address is outside the memory in use in PopToAddress");
+  }
+  while (arena->position_ > _getPageSize() && reduceCondition(arena->committed_size_, arena->position_)) {
+    if (ReduceCommit_LinkedVArena(arena, reducePolicy(arena->committed_size_)) == ERROR_OS_MEMORY) {
+      DEBUG_PRINT("Reduce commit in Virtual arena failed");
+    }
   }
   return SUCCESS;
 }
-int Clear_StaticArena(LinkedArena* arena) {
+int Clear_LinkedVArena(LinkedVArena* arena) {
 #ifdef DEBUG
   if (arena == NULL) {
     return ERROR_INVALID_PARAMS;
   }
 #endif
   arena->position_ = 0;
+  if (ReduceCommit_LinkedVArena(arena, _getPageSize()) == ERROR_OS_MEMORY) {
+    DEBUG_PRINT("Reduce commit in Virtual arena failed");
+  }
   return SUCCESS;
 }
 
 // Essentially, the scratch space is another arena of the same type rooted at the top pointer. Only works for static I guess.
-int InitScratch_StaticArena(LinkedArena* scratch_space, LinkedArena* arena, int arena_size, int auto_align) {
+int InitScratch_LinkedVArena(StaticArena* scratch_space, LinkedVArena* arena, int arena_size, int auto_align) {
 #ifdef DEBUG
   if (scratch_space == NULL || scratch_space == NULL) {
     return ERROR_INVALID_PARAMS;
   }
 #endif
-  uint8_t* mem = PushNoZero_StaticArena(arena, arena_size);
+  uint8_t* mem = PushNoZero_LinkedVArena(arena, arena_size);
   if (mem == NULL) {
     return ERROR_OS_MEMORY;
   }
@@ -219,8 +328,8 @@ int InitScratch_StaticArena(LinkedArena* scratch_space, LinkedArena* arena, int 
   }
   return SUCCESS;
 }
-int DestroyScratch_StaticArena(LinkedArena* scratch_space, LinkedArena* parent_arena) {
-  // Destructor must run under locked mutex of parent to make sure of correct behaviour.
+int DestroyScratch_LinkedVArena(StaticArena* scratch_space, LinkedVArena* parent_arena) {
+  // Make sure you destroy arenas in reverse order on which you created them for correctness.
   // Check for position overflow in the memory pop.
   if (parent_arena->position_ < scratch_space->total_size_) {
     parent_arena->position_ = scratch_space->total_size_;
@@ -234,7 +343,8 @@ int DestroyScratch_StaticArena(LinkedArena* scratch_space, LinkedArena* parent_a
   scratch_space->alignment_  = 0;
   return SUCCESS;
 }
-int MergeScratch_StaticArena(LinkedArena* scratch_space, LinkedArena* parent_arena) {
+int MergeScratch_LinkedVArena(StaticArena* scratch_space, LinkedVArena* parent_arena) {
+  // Merger must run under locked mutex of parent to make sure of correct behaviour.
   // Set the new position to conserve the memory from the scratch space and null properties
   // No need to do bounds check as the memory addresses must be properly ordered, and the position too.
   parent_arena->position_    = ((uintptr_t)scratch_space->memory_ - (uintptr_t)parent_arena->memory_) + scratch_space->position_;
